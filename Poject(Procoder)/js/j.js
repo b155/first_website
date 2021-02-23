5d9a7779497b8df5506e9cfe308192303edf5a48
yeasin arafat <79481994+b155@users.noreply.github.com>
2021-02-23 18:18:19 +0600

    Add files via upload

---
+++ Poject(Procoder)/js/j.js
@@ -0,0 +1,10 @@
+'use strict';
+ 
+//bax slider
+$(document).ready(function(){
+  $('.slider').bxSlider();
+    $('.heder_mene_manu').meanmenu({
+        meanMenuContainer:'#mobile_menu',
+        meanScreenWidth:991,
+    });
+});
